eff07c284e0e6fa0b74e6232f2566e1a2aae3423
juan <juan@juan>
2023-04-26 17:07:27 -0300

    Control Point commit, I will re-build the logic for the fn that appends the selected projects information

--- app.js
+++ app.js
@@ -1,78 +1,215 @@
-import {ProjectOne} from './projects-storage.js'
+import {Projects} from './projects-storage.js'
 
-let projsBtns = document.querySelectorAll('.project-btn');
 let newProjBtn = document.querySelector('.new-project-btn');
 let viewProjsBtn = document.querySelector('.project-all-btn');
 
-
 let asideBar = document.querySelector('aside');
+let btnsContainer = document.querySelector('.project-btns-container');
 let taskViewer = document.querySelector('.task-viewer');
 
-let globalProjCounter = 2;
+let lastProjId = 2;
 
+newProjBtn.addEventListener('click', (event) =>{
 
-projsBtns.forEach((btn) =>{    
+    let newBlankProjName = prompt('Enter a name for the new project');
 
-    btn.addEventListener('click', (event) =>{       
-    
-        taskViewer.innerHTML +=
-    ` 
-        <div class="project-one">
-            
-            <div class="task-info">
-                <div class="id">ID: ${ProjectOne.projId}</div>
-                <div class="due-date">Due Date: ${ProjectOne.projDueDate}</div>  
-            </div>
-    
-            <textarea class="task-content"> ${ProjectOne.taskInfo}
-            </textarea>
-    
-        </div>
-        <button class="complete-task-btn">Mark as Complete</button>
-        <button class="delete-task-btn">Delete task</button>
-        
-        <button class="save-task-btn"> Save task</button>
-        <button class="close-task-btn"> close Task</button>
+    let newProjBtnElem = document.createElement('button');
 
-    `     
+    newProjBtnElem.classList.add('project-btn');
 
-        if(firstEach){
-            let saveTaskBtn = document.querySelector('.save-task-btn');
+    newProjBtnElem.id = lastProjId + 1;
 
-            saveTaskBtn.addEventListener('click', (e) =>{
+    newProjBtnElem.innerHTML = newBlankProjName;
 
-                let taskNewDueDate = document.querySelector('.project-one  .task-info  .id');
-                let taskInfo = document.querySelector('.project-one .task-content');
+    btnsContainer.appendChild(newProjBtnElem);
 
-                console.log(taskNewDueDate.innerHTML);
-                console.log(taskInfo.value);       
-
-                
-
-                
-                
-                console.log(ProjectOne.projId + ' ' + ProjectOne.taskInfo);           
-                
-                
-            })
-        }
-
-        
-    
-
-        
-    })
-        
-
-})
-
-
-
-newProjBtn.addEventListener('click', (e) =>{
-
-    //ver bien esto ,pero cada task nueva tiene que generar un nuevo numero de ID al ser creada
-
-    console.log('prueba new proj btn');  
+    aber();     
        
 })
+
+let aber = () =>{
+
+    //esto queda aca dentro ya que asi cada vez que llamo a aber, se esta renovando el htmlcollection
+    let projsBtns = document.querySelectorAll('.project-btn');
+    
+        projsBtns.forEach((btn) =>{    
+
+            btn.addEventListener('click', (event) =>{  
+        
+                if(event.target.innerHTML == 'Proyecto 1'){        
+                    
+                    taskViewer.innerHTML = 
+                        ` 
+                        <div class="project-one">
+                            
+                            <div class="tasks-list">
+
+                                <div class="task-one">
+
+                                    <div class="task-info">
+                                        <div class="id">ID:${Projects.ProjectOne.tasks.taskOne.taskId}</div>
+                                        <input type="date" min="2023-01-01" max="2023-12-12" class="due-date" id="1">Due Date: a</input>
+                                        <div class="proj-title"></div>  
+                                    </div>
+                                
+                                    <textarea class="task-content">${Projects.ProjectOne.tasks.taskOne.taskInfo}</textarea>
+                                </div>       
+
+                            </div>
+
+                        </div>
+
+                        <button class="complete-task-btn">Mark as Complete</button>
+                        <button class="delete-task-btn">Delete task</button>
+                        
+                        <button class="save-task-btn"> Save task</button>
+                        <button class="close-task-btn"> close Task</button>
+                
+                        `;
+                    
+                    let saveTaskBtn = document.querySelector('.save-task-btn');
+        
+                    saveTaskBtn.addEventListener('click', (e) =>{
+        
+                        let taskNewDueDate = document.querySelector('.project-one  .task-info  .id');
+                        let taskInfoInHtml = document.querySelector('.project-one .task-content');
+        
+                        Projects.ProjectOne.tasks.taskOne.taskInfo = taskInfoInHtml.value;
+                                //el problema es que en el html, el textarea realmente no tiene ningun innerhtml
+                        
+        
+                               
+                        
+                    })
+                
+                    let closeTaskBtn = document.querySelector('.close-task-btn');
+        
+                    closeTaskBtn.addEventListener('click', (e) =>{
+        
+                        taskViewer.innerHTML = '';
+                    })
+        
+                }   else if (event.target.innerHTML == 'Proyecto 2') {
+        
+                   
+                    taskViewer.innerHTML =
+
+                        ` 
+                        <div class="project-two">
+                            
+                            <div class="task-info">
+                                <div class="id">ID:${ProjectTwo.projId}</div>
+                                <input type="date" min="2023-01-01" max="2023-12-12" class="due-date" id="2">Due Date: ${ProjectTwo.projDueDate}</input>  
+                            </div>
+                    
+                            <textarea class="task-content">${ProjectTwo.taskInfo}</textarea>
+                    
+                        </div>
+                        <button class="complete-task-btn">Mark as Complete</button>
+                        <button class="delete-task-btn">Delete task</button>
+                        
+                        <button class="save-task-btn"> Save task</button>
+                        <button class="close-task-btn"> close Task</button>
+                
+                        `;
+                    
+                    let saveTaskBtn = document.querySelector('.save-task-btn');
+        
+                    saveTaskBtn.addEventListener('click', (e) =>{
+        
+                        let taskNewDueDate = document.querySelector('.project-two  .task-info  .id');
+                        let taskInfoInHtml = document.querySelector('.project-two .task-content');
+        
+                        ProjectTwo.taskInfo = taskInfoInHtml.value;
+                               
+                        console.log('taskinfo propiedad ProjectOne: ' + ProjectTwo.taskInfo);
+                        console.log('taskinfo elemento html: ' + taskInfoInHtml.taskInfoInHtml);
+                        console.log('texto del textarea por Value: ' + taskInfoInHtml.value);
+        
+                               
+                        
+                    })
+                
+                    let closeTaskBtn = document.querySelector('.close-task-btn');
+        
+                    closeTaskBtn.addEventListener('click', (e) =>{
+        
+                        taskViewer.innerHTML = '';
+                    })
+        
+        
+                }   else if(event.target.innerHTML == 'caquita'){                    
+        
+                   
+                    taskViewer.innerHTML =
+
+                        ` 
+                        <div class="project-two">
+                            
+                            <div class="task-info">
+                                <div class="id">ID:${ProjectTwo.projId}</div>
+                                <input type="date" min="2023-01-01" max="2023-12-12" class="due-date" id="2">Due Date: ${ProjectTwo.projDueDate}</input>  
+                            </div>
+                    
+                            <textarea class="task-content">${ProjectTwo.taskInfo}</textarea>
+                    
+                        </div>
+                        <button class="complete-task-btn">Mark as Complete</button>
+                        <button class="delete-task-btn">Delete task</button>
+                        
+                        <button class="save-task-btn"> Save task</button>
+                        <button class="close-task-btn"> close Task</button>
+                
+                        `;
+                    
+                    let saveTaskBtn = document.querySelector('.save-task-btn');
+        
+                    saveTaskBtn.addEventListener('click', (e) =>{
+        
+                        let taskNewDueDate = document.querySelector('.project-two  .task-info  .id');
+                        let taskInfoInHtml = document.querySelector('.project-two .task-content');
+        
+                        ProjectTwo.taskInfo = taskInfoInHtml.value;
+                               
+                        console.log('taskinfo propiedad ProjectOne: ' + ProjectTwo.taskInfo);
+                        console.log('taskinfo elemento html: ' + taskInfoInHtml.taskInfoInHtml);
+                        console.log('texto del textarea por Value: ' + taskInfoInHtml.value);
+        
+                               
+                        
+                    })
+                
+                    let closeTaskBtn = document.querySelector('.close-task-btn');
+        
+                    closeTaskBtn.addEventListener('click', (e) =>{
+        
+                        taskViewer.innerHTML = '';
+                    })
+                
+                
+                } else console.log('no entra en el if de los proyectos');  
+                
+                       
+               
+                
+        
+               //hay que hacer que esto se ejecute 1 sola vez de alguna forma, ya que por cada boton se está haciendo.
+        
+                
+            
+        
+                
+            })
+                
+        
+        })
+    
+    
+}
+aber();
+
+
+
+
+
 
--- projects-storage.js
+++ projects-storage.js
@@ -1,16 +1,44 @@
 /* 
 aca podria estar tanto el almacenamiento de las notas y su info
+
+    la nomeclatura de las ID será 1XXX, 1 siendo el proyecto y las X el numero de tarea.
+
 */
 
-let ProjectOne = {
-    //la nomeclatura será 1XXX, 1 siendo el proyecto y las X el numero de tarea.
-    projId : 10001,
-    projDueDate : '01/01/2024',
+let Projects = 
+{
+    ProjectOne : 
+    {
+        tasks : 
+        {
+            taskOne :
+            {   
+                taskId: '1001',
+                taskDueDate : '01/01/2024',
+                taskTitle: 'Primer titulo',
+                taskInfo: 'Primera task informacion'
 
-    taskInfo : ""  
+            }
+        }      
+
+    },
+
+    ProjectTwo :
+    {
+        tasks:
+        {
+            taskOne:
+            {
+                projId : 10002,
+                projDueDate : '01/02/2024',
+                projTitle: '',
+                taskInfo : "Prueba info Task 2"     
+            }
+        }
+    }
 
     
 }
 
 
-export {ProjectOne}
+export {Projects}
